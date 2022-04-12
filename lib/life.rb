3069ac8f49ea59632ad83e6613d9c5ccb0f5615b
Taylor Keazirian <tkeazirian@8thlight.com>
2022-04-12 13:15:51 -0500

    A Universe has a size
    co-authored-by: "Taylor Keazirian" <tkeazirian@8thlight.com>

--- lib/life.rb
+++ lib/life.rb
@@ -1,6 +1,18 @@
 class GameOfLife
-
   def self.hello_world
     "Hello, World!"
+  end
+end
+
+class Universe
+  attr_accessor :width, :height
+
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+
+  def cell_count
+    @width * @height
   end
 end
--- spec/life_spec.rb
+++ spec/life_spec.rb
@@ -5,5 +5,22 @@ describe 'hello_world' do
   it 'prints a hello message' do
     expect(GameOfLife.hello_world).to eq("Hello, World!")
   end
+end
 
+describe 'The Universe' do
+  [
+    [1, 1, 1],
+    [2, 2, 4],
+    [4, 4, 16],
+    [50, 50, 2500],
+    [1000, 1000, 1000000]
+  ].each do |test_width, test_height, expected_cell_count|
+    it "is a grid of cells #{test_width}x#{test_height} cells of size #{expected_cell_count}" do
+      universe = Universe.new(test_width, test_height)
+
+      actual_cell_count = universe.cell_count()
+
+      expect(expected_cell_count).to eq(actual_cell_count)
+    end
+  end
 end
