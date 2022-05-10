631cea270cb0b2ea80c9f9cc3aff6565b1c17293
Taylor Keazirian <tkeazirian@8thlight.com>
2022-05-10 13:25:01 -0500

    moved neighbor logic to cell class
    Co-authored-by: Ian Gagorik <Aoxian@users.noreply.github.com>

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,3 +1,5 @@
+OFFSET = 1
+
 class Cell
   attr_accessor :x, :y
 
@@ -8,5 +10,55 @@ class Cell
 
   def == other 
     x == other.x && y == other.y
+  end
+
+  def neighbor_count(universe)
+    [ n_neighbor?(universe), s_neighbor?(universe),
+      e_neighbor?(universe), w_neighbor?(universe),
+      ne_neighbor?(universe), nw_neighbor?(universe),
+      se_neighbor?(universe), sw_neighbor?(universe)
+    ].count(true)
+  end
+
+  def n_neighbor?(universe)
+    return universe_contains_neighbor(universe, Cell.new(@x, @y + OFFSET))
+  end
+
+  def s_neighbor?(universe)
+    return universe_contains_neighbor(universe, Cell.new(@x, @y - OFFSET))
+  end
+
+  def e_neighbor?(universe)
+    return universe_contains_neighbor(universe, Cell.new(@x + OFFSET, @y))
+  end
+
+  def w_neighbor?(universe)
+    return universe_contains_neighbor(universe,  Cell.new(@x - OFFSET, @y))
+  end
+
+  def nw_neighbor?(universe)
+    return universe_contains_neighbor(universe, Cell.new(@x - OFFSET, @y + OFFSET))
+  end
+
+  def ne_neighbor?(universe)
+    return universe_contains_neighbor(universe, Cell.new(@x + OFFSET, @y + OFFSET))
+  end
+
+  def sw_neighbor?(universe)
+    return universe_contains_neighbor(universe, Cell.new(@x - OFFSET, @y - OFFSET))
+  end
+
+  def se_neighbor?(universe)
+    return universe_contains_neighbor(universe, Cell.new(@x + OFFSET, @y - OFFSET))
+  end
+
+  private
+
+  def universe_contains_neighbor(universe, neighbor_cell)
+    universe.each do |live_cell|
+      return true if live_cell == neighbor_cell
+    end
+
+    return false
   end
 end
--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,4 +1,3 @@
-OFFSET = 1
 
 class GameOfLife
   attr_accessor :width, :height
@@ -19,87 +18,5 @@ class GameOfLife
 
   def live_cell_count(universe)
     universe.size
-  end
-
-  def n_neighbor?(universe, cell)
-    return universe_contains_neighbor(universe, n_neighbor(cell))
-  end
-
-  def s_neighbor?(universe, cell)
-    return universe_contains_neighbor(universe, s_neighbor(cell))
-  end
-
-  def e_neighbor?(universe, cell)
-    return universe_contains_neighbor(universe, e_neighbor(cell))
-  end
-
-  def w_neighbor?(universe, cell)
-    return universe_contains_neighbor(universe, w_neighbor(cell))
-  end
-
-  def nw_neighbor?(universe, cell)
-    return universe_contains_neighbor(universe, nw_neighbor(cell))
-  end
-
-  def ne_neighbor?(universe, cell)
-    return universe_contains_neighbor(universe, ne_neighbor(cell))
-  end
-
-  def sw_neighbor?(universe, cell)
-    return universe_contains_neighbor(universe, sw_neighbor(cell))
-  end
-
-  def se_neighbor?(universe, cell)
-    return universe_contains_neighbor(universe, se_neighbor(cell))
-  end
-
-  def neighbor_count(universe, cell)
-    [ n_neighbor?(universe, cell), s_neighbor?(universe, cell),
-      e_neighbor?(universe, cell), w_neighbor?(universe, cell),
-      ne_neighbor?(universe, cell), nw_neighbor?(universe, cell),
-      se_neighbor?(universe, cell), sw_neighbor?(universe, cell)
-    ].count(true)
-  end
-  
-  private
-
-  def universe_contains_neighbor(universe, neighbor_cell)
-    universe.each do |live_cell|
-      return true if live_cell == neighbor_cell
-    end
-
-    return false
-  end
-
-  def n_neighbor(cell)
-    Cell.new(cell.x, cell.y + OFFSET)
-  end
-  
-  def s_neighbor(cell)
-    Cell.new(cell.x, cell.y - OFFSET)
-  end
-
-  def e_neighbor(cell)
-    Cell.new(cell.x + OFFSET, cell.y)
-  end
-
-  def w_neighbor(cell)
-    Cell.new(cell.x - OFFSET, cell.y)
-  end
-
-  def nw_neighbor(cell)
-    Cell.new(cell.x - OFFSET, cell.y + OFFSET)
-  end
-
-  def ne_neighbor(cell)
-    Cell.new(cell.x + OFFSET, cell.y + OFFSET)
-  end
-
-  def sw_neighbor(cell)
-    Cell.new(cell.x - OFFSET, cell.y - OFFSET)
-  end
-
-  def se_neighbor(cell)
-    Cell.new(cell.x + OFFSET, cell.y - OFFSET)
   end
 end
